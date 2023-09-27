e1b8fc72fac6726ada475fc8cffe2d61de609267
victoria-kirichenko <ruklochka2@gmail.com>
2023-09-27 20:37:51 +0700

    check voice comment

---
+++ checkvoice/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        // voice:.idea/1695821328532.wav
+    }
+}
